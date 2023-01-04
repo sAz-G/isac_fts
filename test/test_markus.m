f5eb2207af4cdbbd825ef878b21de7f535e2fdc4
Mardank <md.krantzik@web.de>
2023-01-04 10:10:13 +0100

    Continued to implement a first variant of the algorithm

--- test/test_markus.m
+++ test/test_markus.m
@@ -83,24 +83,112 @@ while (E_m > Emin)
     % Initialization
     ...
 
-    % Optimization
-    % cvx_begin
-    %     variable S(n)
-    %     variable x(n)
-    %     variable y(n)
-    %     variable V(n)
-    %     variable delta(n)
-    %     variable xi(n)
-    %     
-    %     subject to
-    %         Em >= T_f * ...
-    %         Xi >= 0;
-    %         for i = 1:N_m
-    %             abs(V_last(i))^2 / v_0^2 + 2/v_0 * V_last.'* (V(i) - V_last(i))  >= 1/delta_last(i)^2 - Xi(i);
-    %             delta_last(i)^2 + 2 * delta_last(i) * (delta(i) - delta_last) >= Xi(i)
-    %         end
-    %         
-    % cvx_end
+    %% Optimization
+    cvx_begin
+        variable S(2,n)
+        variable x(n)
+        variable y(n)
+        variable V(n)
+        variable delta(n)
+        variable xi(n)
+        variable d_c(n)
+        variable omega_c(n)
+
+        %% Calculation of the CRB
+        factor_CRB = (P*G_p*beta_0)/(a*sigma_0^2);
+
+        x_km = 
+        x_km_last =
+        x_km = 
+        x_km_last =
+
+        x_last_diff = (x_km - x_km_last);
+        y_last_diff = (y_km - y_km_last);
+
+        d_s_km = sqrt(H^2 + x_last_diff^2 + y_last_diff^2);
+        
+        x_t_est = 
+        y_t_est = 
+        
+        d_s_vec = sqrt(H^2 + x_last_diff.^2 + y_last_diff.^2);
+        
+        x_diff_vec = S(1, :) - x_t_est;
+        y_diff_vec = S(2, :) - y_t_est;
+
+        sum_theta_a = sum_theta_a + sum(factor_CRB * x_diff_vec.^2 ./ (d_s_vec.^6) + 8 * x_diff_vec.^2 ./ (d_s_vec.^6) );
+        sum_theta_b = sum_theta_b + sum(factor_CRB * y_diff_vec.^2 ./ (d_s_vec.^6) + 8 * y_diff_vec.^2 ./ (d_s_vec.^6) );
+        sum_theta_c = sum_theta_c + sum(factor_CRB * x_diff_vec.*y_diff_vec ./ (d_s_vec.^6) + 8 * x_diff_vec.*y_diff_vec ./ (d_s_vec.^6));
+
+        theta_a = sum_theta_a;
+        theta_b = sum_theta_b;
+        theta_c = sum_theta_c;
+
+        CRB_denominator = theta_a * theta_b - theta_c^2;
+
+        derivate_theta_a_part1 = (2* x_last_diff * d_s_km - 6 * x_last_diff^2 * x_last_diff^3)/(d_s_km^8);
+        derivate_theta_a_part2 = (2* x_last_diff * d_s_km - 4 * x_last_diff^2 * x_last_diff^3)/(d_s_km^6);
+
+        derivate_theta_a = factor_CRB * derivate_theta_a_part1 + 8 * derivate_theta_a_part2;
+
+        derivate_theta_b = factor_CRB * y_last_diff^2 * (6 * x_last_diff)/(d_s_km^8);
+        derivate_theta_b = derivate_theta_b + 8 * y_last_diff^2 * (4 * x_last_diff)/(d_s_km^6);
+
+        derivate_theta_c = factor_CRB * y_last_diff * (d_s_km^2 - 6 * x_last_diff^2)/(d_s_km^8);
+        derivate_theta_c = derivate_theta_c + 8 * y_last_diff * (d_s_km^2 - 4 * x_last_diff^2)/(d_s_km^6);
+        
+        derivate_CRB_denominator = derivate_theta_a * theta_b + theta_a * diff_theta_b - 2 * diff_theta_c;
+
+        derivate_CRB_part1 = (derivate_theta_a * CRB_denominator - theta_a * derivate_CRB_denominator)/(CRB_denominator^2);
+        derivate_CRB_part2 = (diff_theta_b * CRB_denominator - theta_b * derivate_CRB_denominator)/(CRB_denominator^2);
+
+        CRB_derivate_x_km = derivate_CRB_part1 + derivate_CRB_part2;
+        
+        % same for y_km
+        CRB_derivate_y_km = ...
+
+        % sum_up for total CRB
+        CRB_affine = CRB_derivate_x_km + CRB_derivate_y_km;
+
+        %% Average Communication Rate
+        sum_Nm = sum_Nm + Nm;
+
+        R_affine = B/sum_Nm * 1/log(2) * 1./(1+omega_c);
+        
+        % sum up over omega_km
+
+        %% Objective function
+        % minimization
+        minimize(nu * CRB_affine - (1 - nu) * R_affine);
+
+        %% Conditions
+        Em_sum1 = sum(P_0 * (1 + 3* abs(V)/(U_tip.^2)) + 0.5 * D_0*rho*s*A*abs(V).^3);
+        Em_sum2 = sum(P_I*delta);
+        Em_sum3 = K_m * (P_0 + P_I);
+
+        subject to
+            Em >= T_f * Em_sum1 + T_F * Em_sum2 + T_h * Em_sum3
+
+            V_max >= abs(V);
+            delta(i) >= 0;
+            S >= 0;
+            S >= 0;
+            L_x >= S;
+            L_y >= S;
+
+            xi >= 0;
+            
+            d_c >= (P * alpha_0) / omega_c;
+
+            for i = 1:N_m
+                
+                abs(V_last(i))^2 / v_0^2 + 2/v_0 * V_last.' * (V(i) - V_last(i)) >= 1/delta_last(i)^2 - Xi(i);
+
+                delta_last(i)^2 + 2 * delta_last(i) * (delta(i) - delta_last) >= Xi(i);
+
+                sqrt(H^2 + norm(S(:,i) - X_c)^2) >= d_c(i);
+            end
+            
+    cvx_end
 
     % Save the opitmized variables
     S_out = [S_out; S];
