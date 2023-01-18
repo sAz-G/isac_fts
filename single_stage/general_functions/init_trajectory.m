aa0aa488ab0e0d20f60cf50f2ca4144f5169dc52
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-01-18 20:15:03 +0100

    update init_trajectory.m
    added comments to the function. changed the calculation of the initial velocity. Changed the name of the outputs. Changed the name of the middle point.

--- single_stage/general_functions/init_trajectory.m
+++ single_stage/general_functions/init_trajectory.m
@@ -1,16 +1,23 @@
-function [S_out, V_out] = init_trajectory(s_start, N_m, x_mid, V_str, T_f)
-%INIT_TRAJECTORY Summary of this function goes here
-%   Detailed explanation goes here
-S_out = zeros(2,N_m);
-V_out = ones(2,N_m) * V_str .* (x_mid - s_start) / norm(x_mid - s_start);
+function [S_init, V_init] = init_trajectory(s_start, N_m, s_mid, V_str, T_f)
+%INIT_TRAJECTORY computes the initial trajectory for the iterative process
+% arguments: 
+% s_start - starting point, the point at which the quad starts.
+% N_m     - the amount of trajectory points at the mth stage.
+% s_mid   - the middle point between the communication user and the
+% estimated sensing target (the estimated position obtained from the
+% radar).
+% V_str   - the speed of the quad. It is constant at throughout the
+% initial trajectory.
+% T_f     - flight time between points.
+% output:
+% S_init  - the initial trajectory.
+% V_out   - the velocity throughout the trajectory.
+
+S_init = zeros(2,N_m);
+V_init  = ones(2,N_m).*V_str.*(s_mid - s_start)./norm(s_mid - s_start);
 
 for n = 1:N_m
-        S_out(:,n) = s_start + V_str * T_f * n * (x_mid - s_start) / norm(x_mid - s_start);
-
-        if n == 1
-            V_out(:,n) = (S_out(:, n) - s_start) / T_f;
-        else
-            V_out(:,n) = (S_out(:, n) - S_out(:, n-1)) / T_f;
-        end
+        % here we multiply by T_f. In the paper they dont, check that point
+        S_init(:,n) = s_start + V_str .* T_f .* n .* (s_mid - s_start) ./ norm(s_mid - s_start);
 end
 
