065dc616ba6839e13e4606ee8f69377a72fe45b5
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-01-29 01:49:17 +0100

    update sigma_k
    added parameters

--- src/radar_sensing_model/sigma_k.m
+++ src/radar_sensing_model/sigma_k.m
@@ -1,3 +1,10 @@
-function sig_k = sigma_k(a,sig_0,P,g_k,Gp)
-    sig_k = (a*(sig_0^2))./(P*Gp*g_k);
+function sig_k = sigma_k(d_s)
+    N_0     = db2pow(-170) * 1e-3;  % [W/Hz]
+    B       = 1e06;                 % channel bandwidth [Hz]
+    sigma_0 = sqrt(B * N_0);        % unitless. Noise power at the receiver 
+    P       = db2pow(20) * 1e-3;    % transmit power [dB]
+    G_p      = 0.1 * B;     % signal processing gain [Hz]
+    a = 10; % pre-determined constant related to the system setting
+
+    sig_k = (a*sigma_0^2)./(P*G_p*g_k(d_s));
 end
