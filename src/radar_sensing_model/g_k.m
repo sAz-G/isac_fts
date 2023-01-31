805cbbc7f347f30714eb021759c5e0ca1b6c2eb3
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-01-31 17:48:49 +0100

    update g_k
    d_s is a vector now. Therefore the dot operator.

--- src/radar_sensing_model/g_k.m
+++ src/radar_sensing_model/g_k.m
@@ -3,6 +3,6 @@ function y = g_k(d_s)
 %   Detailed explanation goes here
 beta_0   = db2pow(-47); % channel power gain in dB at the reference distanc d_s(k) = 1m [W]
 
-y = beta_0./(d_s^4);
+y = beta_0./(d_s.^4);
 end
 
