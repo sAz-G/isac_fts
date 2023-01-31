935ff9aea8875d15ed01a56e3f624d373681a02f
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-01-31 17:50:51 +0100

    update sense_target
    S_q is a vector. d_s is now also a vector.
    
    The function sense_target returns a vector according to the size of S_q

--- src/radar_sensing_model/sense_target.m
+++ src/radar_sensing_model/sense_target.m
@@ -1,7 +1,7 @@
-function d_hat = sense_target(s_t, s_q)
+function d_hat = sense_target(s_t, S_q)
 % S_q quads pos
 % s_t target pos (the real one)
-    d_s = norm(s_t-s_q);
-    d_hat = d_s + sigma_k(d_s)*randn;
+    d_s = norms(s_t-S_q,2);
+    d_hat = d_s + sigma_k(d_s).*randn(1,length(d_s));
 end
 
