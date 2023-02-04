187b97980e792b6a770dd1912e4b2fe904587e49
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-04 18:06:01 +0100

    update sigma_k
    using params instead of defining the variables locally .
    
    return standard deviation instead of the variance

--- src/radar_sensing_model/sigma_k.m
+++ src/radar_sensing_model/sigma_k.m
@@ -1,10 +1,10 @@
-function sig_k = sigma_k(d_s)
-    N_0     = db2pow(-170) * 1e-3;  % [W/Hz]
-    B       = 1e06;                 % channel bandwidth [Hz]
-    sigma_0 = sqrt(B * N_0);        % unitless. Noise power at the receiver 
-    P       = db2pow(20) * 1e-3;    % transmit power [dB]
-    G_p      = 0.1 * B;     % signal processing gain [Hz]
-    a = 10; % pre-determined constant related to the system setting
-
-    sig_k = (a.*sigma_0^2)./(P.*G_p.*g_k(d_s));
+function sig_k = sigma_k(d_s,params)
+    
+    P              = params.sim.P;
+    G_p            = params.sim.G_p;
+    beta_0         = params.sim.beta_0;
+    a              = params.sim.a;
+    sigma_0        = params.sim.sigma_0;
+    
+    sig_k = sqrt( (a.*sigma_0.^2)./( P.*G_p.*g_k(d_s, beta_0) ) );
 end
