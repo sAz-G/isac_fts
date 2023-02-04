5137923c5d5c3191332f20fb74cbe40d3011122b
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-04 18:06:31 +0100

    update g_k
    take parameter beta_0 as argument

--- src/radar_sensing_model/g_k.m
+++ src/radar_sensing_model/g_k.m
@@ -1,7 +1,7 @@
-function y = g_k(d_s)
+function y = g_k(d_s,beta_0)
 %G_K Summary of this function goes here
 %   Detailed explanation goes here
-beta_0   = db2pow(-47); % channel power gain in dB at the reference distanc d_s(k) = 1m [W]
+% beta_0   = db2pow(-47); % channel power gain in dB at the reference distanc d_s(k) = 1m [W]
 
 y = beta_0./(d_s.^4);
 end
