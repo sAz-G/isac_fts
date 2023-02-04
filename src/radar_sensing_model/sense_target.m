2afc5d67cc5524108ca4e7659ee04fc46b36fd86
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-04 18:07:44 +0100

    update sense_target
    take params as argument

--- src/radar_sensing_model/sense_target.m
+++ src/radar_sensing_model/sense_target.m
@@ -1,7 +1,7 @@
-function d_hat = sense_target(s_t, S_q)
+function d_hat = sense_target(s_t, S_q,params)
 % S_q quads pos
 % s_t target pos (the real one)
     d_s = norms(s_t-S_q,2);
-    d_hat = d_s + sigma_k(d_s).*randn(1,length(d_s));
+    d_hat = d_s + sigma_k(d_s,params).*randn(1,length(d_s));
 end
 
