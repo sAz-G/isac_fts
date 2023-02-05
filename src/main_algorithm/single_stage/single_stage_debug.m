06778f2b370d80279ccf4651195998220ad8eeea
Mardank <md.krantzik@web.de>
2023-02-05 16:12:21 +0100

    Adapted single_state_debug.m
    S_s is added in front of S_init so S is now 2 x N_stg+1
    S_s is removed after the stage

--- src/main_algorithm/single_stage/single_stage_debug.m
+++ src/main_algorithm/single_stage/single_stage_debug.m
@@ -1,4 +1,4 @@
-function [S_m, E_used_m, V_m, xi_m, delta_m, CRB_opt, R_opt] = single_stage_debug(E_m,N_stg,delta_square_last,K_stg,s_c,S_init, s_t_est,S_s, V_init, m, params)
+function [S_m, V_m, xi_m, delta_m, CRB_opt, R_opt] = single_stage_debug(E_m,N_stg,delta_square_last,K_stg,s_c,S_init_in, s_t_est,S_s, V_init, m, params)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -29,30 +29,31 @@ debug_V = zeros(2, N_stg, iter);
 debug_delta = zeros(1, N_stg, iter);
 debug_xi = zeros(1, N_stg, iter);
 
+S_init = [S_s, S_init_in];
 
 for u = 1:iter
 % Calculation of the CRB
-S_hover     = S_init(:, mu:mu:N_stg);
-S_hover_x   = S_init(1,mu:mu:N_stg);
-S_hover_y   = S_init(2,mu:mu:N_stg);
+S_hover     = S_init(:, mu+1:mu:N_stg+1);
+S_hover_x   = S_hover(1,:);
+S_hover_y   = S_hover(2,:);
 
 % Derivative with respect to x
-derivatex_CRB = compute_gradient_x(S_hover, s_t_est, H, K_stg, params);
+derivatex_CRB = compute_gradient_crb_x(S_hover, s_t_est, H, K_stg, params);
 
 % Derivative with respect to y
-derivatey_CRB = compute_gradient_y(S_hover, s_t_est, H, K_stg, params);
+derivatey_CRB = compute_gradient_crb_y(S_hover, s_t_est, H, K_stg, params);
 
 % Derivative with respect to x
-derivatex_rate = compute_gradient_rate_x(S_init, s_c, H, N_stg,params);
+derivatex_rate = compute_gradient_rate_x(S_init, s_c, H, N_stg+1,params);
 
 % Derivative with respect to y
-derivatey_rate = compute_gradient_rate_y(S_init, s_c, H, N_stg, params);
+derivatey_rate = compute_gradient_rate_y(S_init, s_c, H, N_stg+1, params);
 
 cvx_begin
     cvx_solver mosek
     cvx_precision high
     
-    variable S(2,N_stg)
+    variable S(2,N_stg+1)
     variable V(2,N_stg)
     variable delta(1,N_stg)
     variable xi(1,N_stg)
@@ -87,13 +88,15 @@ cvx_begin
     %% Conditions
     E_used_constraint = calc_constraint_energy(K_stg,V,delta, params);
     
-    subject to
+    subject to+
         E_m >= E_used_constraint; %T_f * Em_sum1 + T_f * Em_sum2 + T_h * Em_sum3;
         
         V_max >= norms(V, 2, 1);
         delta >= 0;
         
         S(:,1) == S_s;
+        % V_max >= (S(:, 1) - S_s)./T_f;
+
         S(1,:) >= 0;
         S(2,:) >= 0;
 
@@ -104,7 +107,7 @@ cvx_begin
         if u == 1
             xi == delta_square_last;
         end
-        for k = 1:N_stg-1
+        for k = 1:N_stg
             %if k == 1
             %     V(k) == (S(:,k)-S_s)./T_f;
             %else
@@ -123,12 +126,13 @@ cvx_begin
         
 cvx_end
 
-debug_S(:, :, u) = S;
+debug_S(:, :, u) = S(:,2:end);
 debug_V(:, :, u) = V;
 debug_delta(:, :, u) = delta;
 debug_xi(:, :, u) = xi;
 
 S_init = S_init + w_star.*(S-S_init);
+% S_init = S;
 
 V_init = V;
 delta_square_last = delta.^2;
@@ -139,8 +143,7 @@ end
 
 fig_conv = plot_convergence(debug_S, debug_V, debug_delta, debug_xi, S_s, m, params);
 
-S_m = S_init;
-E_used_m = calc_real_energy(K_stg, V, params);
+S_m = S_init(:, 2:end);
 V_m = V;
 xi_m = xi;
 delta_m = delta;
