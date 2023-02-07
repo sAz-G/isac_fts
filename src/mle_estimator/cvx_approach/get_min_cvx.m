7e6ff7624d52c71ae7a907a0cefa3b150a1756ec
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-07 09:59:43 +0100

    create a folder for the cvx approach
    a folder for the cvx approach. It contains only the initial structure

---
+++ src/mle_estimator/cvx_approach/get_min_cvx.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = get_min_cvx(inputArg1,inputArg2)
+%GET_MIN_CVX Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
