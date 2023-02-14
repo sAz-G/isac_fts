78ec9134da0ac631210052dd8efb13dfdbe17390
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-14 15:25:35 +0100

    update get_S_hover
    changed argument list and added the option to get the hover points for stages in a given range

--- src/main_algorithm/multi_stage/get_S_hover.m
+++ src/main_algorithm/multi_stage/get_S_hover.m
@@ -1,14 +1,16 @@
-function [S_hover, hover_idxs] = get_S_hover(S, params, m_start,m_end)
-%GET_S_HOVER extract the hover points out of a given trajectory S given the
-% stage m. The hover points of the trajectory S using the multistage algorithm differ
+function [hover_idxs, S_hover] = get_S_hover( params, m_start,m_end,S)
+%GET_S_HOVER extract the hover points out of a given trajectory S given
+%   range of stages [m_start, m_end].
+% The hover points of the trajectory S when using the multistage algorithm differ
 % from the hover points when calculation the optimal trajectory one time
 % (S_hover_multi_stage(1:end) != S_hover_one_stage, with
 % size(S_hover_multi_stage) == size(S_hover_one_stage).
 %
 % Input: 
-% S      - the trajectory S at the stage m
-% params - simulation and setup parameters
-% m      - stage number  
+% S         - the trajectory S at the stage m
+% params    - simulation and setup parameters
+% m_start   - starting stage number  
+% m_end     - ending stage number 
 %
 % Output:
 % S_hover   - hover points (already visited) up to the (m-1)th stage
@@ -19,13 +21,18 @@ N_stg = params.sim.N_stg;
 mu    = params.sim.mu;
 K_stg = floor(N_stg./mu);
 
-shft                = mod(N_stg, params.sim.mu); 
+shft                = mod(N_stg, mu); 
 shft_vec            = shft*mod((m_start-1+mu):(m_end-1 + mu), mu);
-shft_mat            = ones(K_stg, m_end).*shft_vec;
+shft_mat            = ones(K_stg, m_end-m_start+1).*shft_vec;
 idxs_shift          = shft_mat(:)'; 
-hover_idxs_linear   = [mu:mu:(m_end*N_stg)];
-hover_idxs          = idxs_shift + hover_idxs_linear(1:m_end*K_stg);
-S_hover             = S(:,hover_idxs);
+hover_idxs_linear   = [mu+(m_start-1)*K_stg*mu:mu:(m_end*K_stg*mu)];
+hover_idxs          = idxs_shift + hover_idxs_linear;
+
+if ~ isempty(S)
+    S_hover             = S(:,hover_idxs);
+else
+    S_hover = [];
+end
 
 end
 
