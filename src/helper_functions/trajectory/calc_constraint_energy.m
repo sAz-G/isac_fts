c992812962199aeac2d94006bae529c56c7b3582
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-02-27 17:45:58 +0100

    update calc_constraint_energy
    change argument list:
       - K_stg can be taken from params
    
    give variables their correct name:
    - power instead of energy
    
    make code with less operations, in particular less multiplications.
    
    clean code - add comments and make and add spacing to make code easier to read

--- src/helper_functions/trajectory/calc_constraint_energy.m
+++ src/helper_functions/trajectory/calc_constraint_energy.m
@@ -1,4 +1,4 @@
-function E_used = calc_constraint_energy(K_stg,V,delta,params)
+function E_used = calc_constraint_energy(V,delta,params)
 % calc_energy calculates the energy, given the previous energy, and the
 % energy used in each stage, which is predefined.
 % Arguments: 
@@ -16,13 +16,16 @@ U_tip = params.energy.U_tip;
 P_0   = params.energy.P_0;
 P_I   = params.energy.P_I;
 
-T_f = params.sim.T_f;
-T_h = params.sim.T_h;
+T_f   = params.sim.T_f;
+T_h   = params.sim.T_h;
+K_stg = params.sim.K_stg;
 
-Em_sum1 = sum(P_0 * (1 + 3 * pow_pos(norms(V,2,1), 2)/(U_tip.^2)) + 0.5 * D_0 * rho * s * A * pow_pos(norms(V, 2, 1), 3));
-Em_sum2 = sum(P_I*delta);
-Em_sum3 = K_stg* (P_0 + P_I);
-E_used = T_f * Em_sum1 + T_f * Em_sum2 + T_h * Em_sum3;
+power_sum1 = P_0.*(size(V,2) + 3./(U_tip.^2).*sum( pow_pos( norms(V,2,1),2) ) );
+power_sum1 = power_sum1 + 0.5*D_0*rho*s*A*sum(pow_pos(norms(V,2,1),3));
+
+power_sum2 = P_I.*sum(delta);
+power_sum3 = K_stg.*(P_0+P_I);
+E_used     = T_f*(power_sum1+power_sum2)+T_h*power_sum3;
  
 
 end
