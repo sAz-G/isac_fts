5d0922eef0b280f40dd2f9ea3c3ee947bc15bfef
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-03-03 11:31:02 +0100

    update optimize_m_debug
    change argument list. Initial values based on the initial trajectory is calculated in this function.
    
    The initial trajectory is extracted from the total trajectory S_total.
    
    add comments and update buffer structure.
    
    update function call of rate_grad.
    
    reorder constraint list.
    
    handle nan values in the result. (basically return the previous results and stop optimization).

--- src/main_algorithm/single_stage/optimize_m_debug.m
+++ src/main_algorithm/single_stage/optimize_m_debug.m
@@ -1,4 +1,4 @@
-function [S_m, V_m, xi_m, delta_m, CRB_iter, R_iter] = optimize_m_debug(E_m, s_c,S_hover, S_total, delta_square_init,s_t,s_s, V_init, params)
+function [S_m, V_m, xi_m, delta_m, CRB_iter, R_iter] = optimize_m_debug(E_m, s_c,S_hover, S_total,s_t,s_s, params)
 % optimize_m_debug optimizes the trajectory for given initial values at the
 % mth stage
 %   
@@ -10,9 +10,6 @@ function [S_m, V_m, xi_m, delta_m, CRB_iter, R_iter] = optimize_m_debug(E_m, s_c
 %                     hover points of the mth stage are going to be optimized.
 % S_total           - all trajectory points including these of the mth stage. The
 %                     points of the mth stage are going to be optimized.
-% delta_square_init - initial delta square value passed to the function. It changes in every iteration
-%                     of the optimization algorithm 
-% V_init            - initial velocity 
 % params            - constants that are defined before running the whole program
 %
 % return:
@@ -32,18 +29,24 @@ w_star  = params.sim.w_star; % step size
 N_stg   = params.sim.N_stg;  % amount of trajectory points at each stage 
 K_stg   = params.sim.K_stg;   % amount of trajectory hover points at each stage 
 
-CRB_iter = nan(1,iter);       % store crb values of each iteration 
-R_iter   = nan(1,iter);       % store rate values of each iteration 
+CRB_iter = nan(1,1);       % store crb values of each iteration 
+R_iter   = nan(1,1);       % store rate values of each iteration 
 
 %% Debug parameter 
 
-S_mat     = zeros(2, N_stg, iter); % store trajectory results of each iteration
-V_mat     = zeros(2, N_stg, iter); % store velocity results of each iteration
-delta_mat = zeros(N_stg, iter);    % store delta results of each iteration 
-xi_mat    = zeros(N_stg, iter);    % store xi results of each iteration 
+S_mat     = zeros(2, N_stg, 1); % store trajectory results of each iteration
+V_mat     = zeros(2, N_stg, 1); % store velocity results of each iteration
+delta_mat = zeros(N_stg, 1);    % store delta results of each iteration 
+xi_mat    = zeros(N_stg, 1);    % store xi results of each iteration 
 
-S_init = S_total(:,end-N_stg+1:end); % initial trajectory points. These points are going to be optimized
-
+% initial values 
+S_init = S_total(:,end-N_stg+1:end); 
+V_init = nan(size(S_init));
+V_init(:,1)       = (S_init(:,1)-s_s)./params.sim.T_f;
+V_init(:,2:end)   = (S_init(:,2:end)-S_init(:,1:end-1))./params.sim.T_f;
+delta_square_init = sqrt(1 + norms(V_init, 2, 1).^4/(4*params.energy.v_0^4))...
+                        - norms(V_init, 2 ,1).^2/(2*params.energy.v_0^2);
+                    
 for u = 1:iter  % optimization loop
 
 % assign optimized hover points and then optimized trajectory
@@ -57,10 +60,10 @@ crb_grad_x = crb_grad(S_hover, s_t, params, 'x');
 crb_grad_y = crb_grad(S_hover, s_t, params, 'y'); 
  
 % gradient of rate in x direction 
-rate_grad_x = rate_grad(S_total, s_c, params, 'x');
+rate_grad_x = rate_grad(S_total(:,end-N_stg+1:end), s_c, params, 'x',size(S_total,2));
 
 % gradient of rate in y direction 
-rate_grad_y = rate_grad(S_total, s_c, params, 'y');
+rate_grad_y = rate_grad(S_total(:,end-N_stg+1:end), s_c, params, 'y',size(S_total,2));
 
 cvx_begin % cvx entry point
     cvx_solver mosek    % use solver mosek
@@ -81,9 +84,9 @@ cvx_begin % cvx entry point
     CRB_taylor =  crb_taylor_x + crb_taylor_y;
 
     % first degree of taylor series expansion in x direction
-    rate_taylor_x = sum(rate_grad_x .* (S(1,:) - S_init(1,:))); 
+    rate_taylor_x = sum(rate_grad_x.*(S(1,:) - S_init(1,:))); 
     % first degree of taylor series expansion in y direction
-    rate_taylor_y = sum(rate_grad_y .* (S(2,:) -  S_init(2,:))); 
+    rate_taylor_y = sum(rate_grad_y.*(S(2,:) -  S_init(2,:))); 
     
     % first degree taylor series expansion of the rate
     R_taylor =  rate_taylor_x + rate_taylor_y;
@@ -91,15 +94,14 @@ cvx_begin % cvx entry point
 
     %% Objective function
     % minimization
-    minimize(eta.*CRB_taylor-(1-eta).*R_taylor);
-
+    minimize(eta.*(CRB_taylor) -(1-eta).*R_taylor);
     %% constraints
     
     subject to
         E_m     >= calc_constraint_energy(V,delta, params); % energy constraint
+        (S(:,1) - s_s)./params.sim.T_f == V(:,1); % definition of the velocity
+        (S(:,2:end) - S(:,1:end-1))./params.sim.T_f == V(:,2:end); % definition of the velocity
         V_max   >= norms(V, 2, 1);    % velocity constraint
-        (S(:,1) - s_s)./params.sim.T_f == V(:,1);
-        (S(:,2:end) - S(:,1:end-1))./params.sim.T_f == V(:,2:end);
         delta   >= 0;                 % delta is not negative 
         xi      >= 0;                 % xi is not negative
         S(1,:)  >= 0;                 % all x points are not negative                                                      
@@ -114,32 +116,33 @@ cvx_begin % cvx entry point
         
 cvx_end % end of cvx calculations 
 
-if u < iter % set an initial trajectory for the next iteration  
-    S_init            = S_init + w_star.*(S-S_init);
-    V_init(:,1)       = (S_init(:,1)-s_s)./params.sim.T_f;
-    V_init(:,2:end)   = (S_init(:,2:end)-S_init(:,1:end-1))./params.sim.T_f;
-    delta_square_init = sqrt(1 + norms(V_init, 2, 1).^4/(4*params.energy.v_0^4))...
-                            - norms(V_init, 2 ,1).^2/(2*params.energy.v_0^2);
-else % set S_init to the cvx result at the last iteration 
-    S_init = S;
-end
-
-%V_init = V;                             % set initial velocity for the next iteration 
-%delta_square_init = pow_pos(delta,2);   % set initial delta_square for the next iteration
-
-S_mat(:, :, u) = S_init;      % store trajectory results of each iteration 
-V_mat(:, :, u) = V;           % store velocity results of each iteration 
-delta_mat(:, u) = delta;   % store detla results of each iteration 
-xi_mat(:, u) = xi;         % store xi results of each iteration 
-
-R_iter(u)    = R_taylor;         % store rate results of each iteration 
-CRB_iter(u)  = CRB_taylor;       % store crb results  of each iteration 
+% still have to check what to do if the cvx result is nan or inf
+if ~isnan(S)
+    if u < iter % set an initial trajectory for the next iteration  
+        S_init            = S_init + w_star.*(S-S_init);
+        V_init(:,1)       = (S_init(:,1)-s_s)./params.sim.T_f;
+        V_init(:,2:end)   = (S_init(:,2:end)-S_init(:,1:end-1))./params.sim.T_f;
+        delta_square_init = sqrt(1 + norms(V_init, 2, 1).^4/(4*params.energy.v_0^4))...
+                                - norms(V_init, 2 ,1).^2/(2*params.energy.v_0^2);
+    else % set S_init to the cvx result at the last iteration 
+        S_init = S;
+    end
+    S_mat(:, :, u) = S;      % store trajectory results of each iteration 
+    V_mat(:, :, u) = V;           % store velocity results of each iteration 
+    delta_mat(:, u) = delta;   % store detla results of each iteration 
+    xi_mat(:, u) = xi;         % store xi results of each iteration 
+    R_iter(1,u)    = R_taylor;         % store rate results of each iteration 
+    CRB_iter(1,u)  = CRB_taylor;       % store crb results  of each iteration 
+    
+    % return output variables. This is the final optimization result
+    S_m         = S_mat(:,:,1:u);      % optimal trajectory 
+    V_m         = V_mat(:,:,1:u);      % optimal velocity
+    xi_m        = xi_mat(:,1:u);     % optimal xi
+    delta_m     = delta_mat(:,1:u);  % optimal delta
+else
+    return
+end % end of if ~isnan(S)
 end % end of for loop and optimization
 
-% return output variables. This is the final optimization result
-S_m         = S_mat;      % optimal trajectory 
-V_m         = V_mat;      % optimal velocity
-xi_m        = xi_mat;     % optimal xi
-delta_m     = delta_mat;  % optimal delta
 end
 
