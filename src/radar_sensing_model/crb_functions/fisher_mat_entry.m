8091a54dde3fe53a605ab6c0faa12ac817896984
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-03-03 11:34:07 +0100

    update fisher_mat_entry
    change argument list. factor_crb can be given as an argument to the function.
    
    change calculation of the theta_c entry. Use a nested function instead of using the previous term, which can introduce complex numbers.

--- src/radar_sensing_model/crb_functions/fisher_mat_entry.m
+++ src/radar_sensing_model/crb_functions/fisher_mat_entry.m
@@ -1,4 +1,4 @@
-function fisher_entry = fisher_mat_entry(S_hov, s_target,entry,params,relative_dist_vec)
+function fisher_entry = fisher_mat_entry(S_hov, s_target,entry,params,relative_dist_vec,factor_CRB)
 %FISHER_MAT_ENTRY Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,15 +6,17 @@ if ~exist('relative_dist_vec', 'var')
         relative_dist_vec         = relative_distance(S_hov, s_target, params.sim.H);
 end
 
+if ~exist('factor_CRB', 'var')
+        P              = params.sim.P;
+        G_p            = params.sim.G_p;
+        beta_0         = params.sim.beta_0;
+        a              = params.sim.a;
+        sigma_0        = params.sim.sigma_0;
+        factor_CRB     = (P.*G_p.*beta_0)/(a*sigma_0.^2);
+end
+
 if strcmp(entry,'theta_a')    
     relative_pos_vec_x = (S_hov(1,:) - s_target(1))';
-    
-    P              = params.sim.P;
-    G_p            = params.sim.G_p;
-    beta_0         = params.sim.beta_0;
-    a              = params.sim.a;
-    sigma_0        = params.sim.sigma_0;
-    factor_CRB     = (P.*G_p.*beta_0)/(a*sigma_0.^2);
     
     left_sumand  = factor_CRB.*(relative_pos_vec_x.'*diag(1./relative_dist_vec.^6)*relative_pos_vec_x);
     right_sumand = 8.*(relative_pos_vec_x.'*diag(1./relative_dist_vec.^4)*relative_pos_vec_x);
@@ -26,17 +28,23 @@ elseif strcmp(entry,'theta_b')
         fisher_mat_entry([S_hov(2,:);S_hov(1,:)], [s_target(2);s_target(1)],...
                          'theta_a',params,relative_dist_vec);
 elseif strcmp(entry,'theta_c')
-    
-    x_entries = sqrt((S_hov(2,:)-s_target(2)).*(S_hov(1,:)-s_target(1)));
-    y_entries = sqrt((S_hov(1,:)-s_target(1)).^2 + (S_hov(2,:)-s_target(2)).^2 - x_entries.^2);
-    
-    fisher_entry =...
-        fisher_mat_entry([x_entries;y_entries], [0,0],...
-                        'theta_a',params,relative_dist_vec);
+    relative_pos_x = (S_hov(1,:) - s_target(1)).';
+    relative_pos_y = (S_hov(2,:) - s_target(2)).';
 
+    fisher_entry = entry_c(relative_pos_x, relative_pos_y, relative_dist_vec,factor_CRB);
+    
 elseif strcmp(entry,'all')
 else
 end
+
+function theta_c = entry_c(relative_pos_x, relative_pos_y, relative_dist_vec,factor_CRB)
+    
+    left  = factor_CRB.*(relative_pos_x.'*diag(1./relative_dist_vec.^6)*relative_pos_y);
+    right = 8.*(relative_pos_x.'*diag(1./relative_dist_vec.^4)*relative_pos_y);
+   
+    theta_c = left + right;
+end
+
 
 end
 
