3df5ca79e26959629570779290831389463c02a6
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-03-03 11:26:28 +0100

    update optimize_m
    update the function based on optimize_m_debug

--- src/main_algorithm/single_stage/optimize_m.m
+++ src/main_algorithm/single_stage/optimize_m.m
@@ -1,126 +1,143 @@
-function [S_m, E_used_m, V_m, xi_m, delta_m, CRB_opt, R_opt] = optimize_m(E_m,N_stg,delta_square_last,K_stg,s_c,S_init, s_t_est,S_s, V_init,params)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [S_opt, V_opt, xi_opt, delta_opt, CRB_opt, R_opt] = optimize_m(E_m, s_c,S_hover, S_total,s_t,s_s, params)
+% optimize_m optimizes the trajectory for given initial values at the
+% mth stage. This function returns the final results. For debugging, see
+% optimize_m_debug
+%   
+% input: 
+% 
+% E_m               - the available energy at the mth stage. This is needed for the constraint. 
+% s_c               - position of the communication user 
+% S_hover           - all hover points including the mth stage. The hover points K
+%                     hover points of the mth stage are going to be optimized.
+% S_total           - all trajectory points including these of the mth stage. The
+%                     points of the mth stage are going to be optimized.
+% params            - constants that are defined before running the whole program
+%
+% return:
+% optimization variables of each iteration 
 
-% parameters 
-L_x   = params.sim.L_x;
-L_y   = params.sim.L_y;
-V_max = params.sim.V_max;
+% constants 
+L_x   = params.sim.L_x;     % boundary in the x direction 
+L_y   = params.sim.L_y;     % boundary in the y direction 
+V_max = params.sim.V_max;   % max possibe velocity
 
-v_0 = params.energy.v_0;
+v_0     = params.energy.v_0;    % energy parameter 
 
-B = params.sim.B;
-R_max = 1; %1e06*log(1 + (db2pow(20)*1e-3*db2pow(-50))./(sqrt(1e06*db2pow(-170) * 1e-3)*H.^2));
+iter    = params.sim.iter;   % amount of iterations for the optimization algo
+mu      = params.sim.mu;     % sense target every mu points. 1,2,mu,...,N_m
+eta     = params.sim.eta;    % trade of constant for the object function 
+w_star  = params.sim.w_star; % step size
+N_stg   = params.sim.N_stg;  % amount of trajectory points at each stage 
+K_stg   = params.sim.K_stg;   % amount of trajectory hover points at each stage 
 
-iter    = params.sim.iter;
-mu      = params.sim.mu;
-H       = params.sim.H;
-eta     = params.sim.eta;
-w_star  = params.sim.w_star;
 
-CRB_opt = nan(1,iter);
-R_opt   = nan(1,iter);
+%% initial values 
+S_init = S_total(:,end-N_stg+1:end); 
+V_init = nan(size(S_init));
+V_init(:,1)       = (S_init(:,1)-s_s)./params.sim.T_f;
+V_init(:,2:end)   = (S_init(:,2:end)-S_init(:,1:end-1))./params.sim.T_f;
+delta_square_init = sqrt(1 + norms(V_init, 2, 1).^4/(4*params.energy.v_0^4))...
+                        - norms(V_init, 2 ,1).^2/(2*params.energy.v_0^2);
+                    
+for u = 1:iter  % optimization loop
 
-for u = 1:iter
-% Calculation of the CRB
-S_hover     = S_init(:, mu:mu:N_stg);
-S_hover_x   = S_init(1,mu:mu:N_stg);
-S_hover_y   = S_init(2,mu:mu:N_stg);
+% assign optimized hover points and then optimized trajectory
+S_hover(:,end-K_stg+1:end)  = [S_init(1, mu:mu:N_stg); S_init(2, mu:mu:N_stg)]; 
+S_total(:,end-N_stg+1:end)  = S_init;
 
-% Derivative with respect to x
-derivatex_CRB = compute_gradient_crb_x(S_hover, s_t_est, H, K_stg, params);
+% gradient of crb in x direction 
+crb_grad_x = crb_grad(S_hover, s_t, params, 'x');
 
-% Derivative with respect to y
-derivatey_CRB = compute_gradient_crb_y(S_hover, s_t_est, H, K_stg, params);
+% gradient of crb in y direction 
+crb_grad_y = crb_grad(S_hover, s_t, params, 'y'); 
+ 
+% gradient of rate in x direction 
+rate_grad_x = rate_grad(S_total(:,end-N_stg+1:end), s_c, params, 'x',size(S_total,2));
 
-% Derivative with respect to x
-derivatex_rate = compute_gradient_rate_x(S_init, s_c, H, N_stg,params);
+% gradient of rate in y direction 
+rate_grad_y = rate_grad(S_total(:,end-N_stg+1:end), s_c, params, 'y',size(S_total,2));
 
-% Derivative with respect to y
-derivatey_rate = compute_gradient_rate_y(S_init, s_c, H, N_stg, params);
+% boundary values for the rate function 
+%R_max = rate_boundary(s_s,s_c,size(S_total,2),params, 'max');
+%R_min = rate_boundary(s_s,s_c,size(S_total,2),params, 'min');
 
-%y_opt = S(2,mu:mu:N_tot); % optimization variable
-
-cvx_begin
-    cvx_solver mosek
-    cvx_precision high
+cvx_begin % cvx entry point
+    cvx_solver mosek    % use solver mosek
+    cvx_precision high  
     
-    variable S(2,N_stg)
-    variable V(2,N_stg)
-    variable delta(1,N_stg)
-    variable xi(1,N_stg)
+    % optimization variables
+    variable S(2,N_stg)     % trajectory 
+    variable V(2,N_stg)     % velocity
+    variable delta(1,N_stg) % delta (see paper)
+    variable xi(1,N_stg)    % xi (see paper)
     
+    % first degree taylor series expansion of crb in x direction 
+    crb_taylor_x = sum(crb_grad_x.*(S(1,mu:mu:N_stg) - S_init(1, mu:mu:N_stg)));
+    % first degree taylor series expansion of crb in y direction
+    crb_taylor_y = sum(crb_grad_y.*(S(2,mu:mu:N_stg) - S_init(2, mu:mu:N_stg))); 
     
-    
-    %x_opt = S(1,mu:mu:N_tot); % optimization variable
+    % first degree taylor series expansion of crb 
+    CRB_taylor =  crb_taylor_x + crb_taylor_y;
 
-    CRB_derivate_x_km = sum(derivatex_CRB .* (S(1,mu:mu:N_stg) - S_hover_x)); % vectorized
-    CRB_derivate_y_km = sum(derivatey_CRB .* (S(2,mu:mu:N_stg) - S_hover_y)); % vectorized
+    % first degree of taylor series expansion in x direction
+    rate_taylor_x = sum(rate_grad_x.*(S(1,:) - S_init(1,:))); 
+    % first degree of taylor series expansion in y direction
+    rate_taylor_y = sum(rate_grad_y.*(S(2,:) -  S_init(2,:))); 
     
-    % sum_up for total CRB
-    CRB_affine =  CRB_derivate_x_km + CRB_derivate_y_km;
-
-    % Average Communication Rate
-    R_derivate_x = sum(derivatex_rate .* (S(1,:) - S_init(1,:))); % vectorized
-    
-    R_derivate_y = sum(derivatey_rate .* (S(2,:) -  S_init(2,:))); % vectorized
-
-    R_affine =  R_derivate_x + R_derivate_y;
+    % first degree taylor series expansion of the rate
+    R_taylor =  rate_taylor_x + rate_taylor_y;
  
 
     %% Objective function
     % minimization
-    minimize(eta * CRB_affine - (1 - eta) * R_affine);
-
-    %% Conditions
-    E_used_constraint = calc_constraint_energy(K_stg,V,delta, params);
+    minimize(eta.*(CRB_taylor)-(1-eta).*R_taylor);
+    %% constraints
     
     subject to
-        E_m >= E_used_constraint;%T_f * Em_sum1 + T_f * Em_sum2 + T_h * Em_sum3;
-        V_max >= norms(V, 2, 1);
-        delta >= 0;
-        S(:,1) == S_s;
-        S(1,:) >= 0;
-        S(2,:) >= 0;
-        L_x >= S(1,:);
-        L_y >= S(2,:);
-        xi >= 0;
-        %R_affine <= R_max;
-        if u == 1
-            xi == delta_square_last;
-        end
-        for k = 1:N_stg-1
-            %if k == 1
-            %     V(k) == (S(:,k)-S_s)./T_f;
-            %else
-            %     V(:,k) == (S(:,k)-S(:,k-1))./T_f;
-            %end
-            norm(S(:,k+1) - S(:,k)) <= V_max*params.sim.T_f;
-           
+        E_m     >= calc_constraint_energy(V,delta, params); % energy constraint
+        (S(:,1) - s_s)./params.sim.T_f == V(:,1); % definition of the velocity
+        (S(:,2:end) - S(:,1:end-1))./params.sim.T_f == V(:,2:end); % definition of the velocity
+        V_max   >= norms(V, 2, 1);    % velocity constraint
+        delta   >= 0;                 % delta is not negative 
+        xi      >= 0;                 % xi is not negative
+        S(1,:)  >= 0;                 % all x points are not negative                                                      
+        S(2,:)  >= 0;                 % all y points are not negative
+        L_x     >= S(1,:);            % all x points are bounded by L_x
+        L_y     >= S(2,:);            % all y points are bounded by L_y
+        %R_taylor <= R_max;
+        %R_taylor >= R_min;
+        for i = 1:N_stg % add constraints 51a and 51b from the paper
+           (norm(V_init(:, i))./v_0).^2 + (2./v_0.^2).*V_init(:, i).'*(V(:,i)-V_init(:,i)) >= pow_pos(inv_pos(delta(i)), 2)-xi(i);
+           delta_square_init(i) + 2.*sqrt(delta_square_init(i)).*(delta(i) - sqrt(delta_square_init(i))) >=  xi(i);
         end
         
-        for i = 1:N_stg
-            
-            norm(V_init(:, i))^2 / v_0^2 + 2/v_0^2 * V_init(:, i).' * (V(:,i) - V_init(:,i)) >= 1/delta_square_last(i) - xi(i);
+cvx_end % end of cvx calculations 
 
-            delta_square_last(i) + 2 * sqrt(delta_square_last(i)) * (delta(i) - sqrt(delta_square_last(i))) >=  xi(i);
-        end
-        
-cvx_end
+% still have to check what to do if the cvx result is nan or inf
 
-S_init = S_init + w_star.*(S-S_init);
+if ~isnan(S)
+    if u < iter % set an initial trajectory for the next iteration  
+        S_init            = S_init + w_star.*(S-S_init);
+        V_init(:,1)       = (S_init(:,1)-s_s)./params.sim.T_f;
+        V_init(:,2:end)   = (S_init(:,2:end)-S_init(:,1:end-1))./params.sim.T_f;
+        delta_square_init = sqrt(1 + norms(V_init, 2, 1).^4/(4*params.energy.v_0^4))...
+                                - norms(V_init, 2 ,1).^2/(2*params.energy.v_0^2);
+    else % set S_init to the cvx result at the last iteration 
+        S_init = S;
+    end
+    % return output variables. assign here because cvx might result in nan
+    % or inf
+    S_opt = S;      % store trajectory results of each iteration 
+    V_opt = V;           % store velocity results of each iteration 
+    delta_opt = delta;   % store detla results of each iteration 
+    xi_opt = xi;         % store xi results of each iteration 
+    R_opt    = R_taylor;         % store rate results of each iteration 
+    CRB_opt  = CRB_taylor;       % store crb results  of each iteration 
+else
+    return;
+end % end of if ~isnan(S)
+end % end of for loop and optimization
 
-V_init = V;
-delta_square_last = delta.^2;
 
-R_opt(u)    = R_affine;
-CRB_opt(u)  = CRB_affine;
-end
-
-S_m = S_init;
-E_used_m = calc_real_energy(S_m, V, params);
-V_m = V;
-xi_m = xi;
-delta_m = delta;
 end
 
