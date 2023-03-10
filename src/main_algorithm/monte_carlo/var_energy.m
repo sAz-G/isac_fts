a7988716ffa37dcd7a8b12b66af25ab368c069bf
Mardank <md.krantzik@web.de>
2023-03-10 22:41:51 +0100

    Updated Monte-Carlo Simulation

--- src/main_algorithm/monte_carlo/var_energy.m
+++ src/main_algorithm/monte_carlo/var_energy.m
@@ -6,7 +6,6 @@ close all;
 addpath(genpath("..\..\..\src"));
 
 % set rng to default
-rng('default');
 
 %% Simulation parameter
 
@@ -19,6 +18,7 @@ L_y = params.sim.L_y;
 
 energy_vec = 10e3:5e3:35e3;
 CRB_over_energy = zeros(1,length(energy_vec));
+Rate_over_energy = zeros(1,length(energy_vec));
 
 counter = 1;
 
@@ -30,12 +30,25 @@ for var_total_energy = energy_vec
 
     % call the monte_carlo function to to the monte-carlo-simulation of the current setup
     res_mc = monte_carlo(params, setup, number_mc_iterations);
-
+    
     % save the avg. CRB
     CRB_over_energy(counter) = res_mc.CRB_avg;
 
+    % save the avg. Rate
+    Rate_over_energy(counter) = res_mc.Rate_avg;
     counter = counter + 1;
 end
 
 %% Evaluate and plot the parameters
-plot(energy_vec, CRB_over_energy)
+figure
+yyaxis left
+semilogy(energy_vec, CRB_over_energy)
+grid on
+
+xlabel("total energy")
+title("MC-Simulation CRB iter=10 eta=1.0")
+ylabel("avg. CRB")
+
+yyaxis right
+semilogy(energy_vec, Rate_over_energy)
+ylabel("avg. Rate")
