9bc764453df96460a21f739e64420267b4b109d7
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-03-10 22:02:49 +0100

    add boundary check
    the function returns now only a valid trajectory.
    
    First a trajectory is calculated as before.
    
    Then, It checks if the end point is out of the region and if so it sets a new one at the boundary but on the same line that is calculated initially

--- src/helper_functions/trajectory/init_trajectory.m
+++ src/helper_functions/trajectory/init_trajectory.m
@@ -15,9 +15,61 @@ function [S_init, V_init] = init_trajectory(s_start, s_end,N, params)
 
 V_str = params.sim.V_str;
 T_f   = params.sim.T_f;
+L_x = params.sim.L_x;
+L_y = params.sim.L_y;
 
 S_init = zeros(2,N);
 V_init  = ones(2,N).*V_str.*(s_end - s_start)./norm(s_end - s_start);
-S_init(:,1:N) = s_start + V_str .*(1:N).* (s_end - s_start)./norm(s_end - s_start)*T_f;
+
+S_init(:,1:N) = s_start + V_str.*(1:N).*(s_end - s_start)./norm(s_end - s_start)*T_f;
+
+% check if the calculated trajectory is valid. if not, calculate a new one.
+s_e = S_init(:,end); % get last position at the initial trajectory
+
+% read comments in first if statement to get an idea what happens in each
+% one of them
+if s_e(1) > L_x                                 % x position x out of bounds
+    x_new  = L_x;                               % calc new x
+    s_e(1) = x_new;                             % calc new x
+    grad   = s_start - s_end;                   % calc diff to obtain slope
+    grad   = grad(2)./grad(1);                  % calc slop 
+    y_new  = s_end(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
+    s_e(2) = y_new;                             % set new x
+
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif s_e(1) < 0
+    x_new = 0;
+    s_e(1) = x_new;
+    grad   = s_start - s_end;
+    grad   = grad(2)./grad(1);
+    y_new  = s_end(2) + grad.*(x_new-s_e(1));
+    s_e(2) = y_new;
+    
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif s_e(2) > L_y
+    y_new     = L_y;
+    s_e(2)    = y_new;
+    grad_inv  = s_start - s_end;
+    grad_inv  = grad_inv(1)./grad_inv(2);
+    x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
+    s_e(1)    = x_new;
+    
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif s_e(2) < 0 
+    y_new    = 0;
+    s_e(2)   = y_new;
+    grad_inv = s_start - s_end;
+    grad_inv = grad_inv(1)./grad_inv(2);
+    x_new    = s_end(1) + grad_inv.*(y_new-s_e(2));
+    s_e(1)   = x_new;
+     
+   S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
+   S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+
+end
+
 end
 
