032e724d02ce638e561c715c387a84bf547cd87d
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-04-10 17:07:04 +0200

    add boundary check

--- src/helper_functions/trajectory/init_trajectory.m
+++ src/helper_functions/trajectory/init_trajectory.m
@@ -1,6 +1,6 @@
 function [S_init, V_init] = init_trajectory(s_start, s_end,N, params)
 %INIT_TRAJECTORY computes the initial trajectory for the iterative process
-% arguments: 
+% arguments:
 % s_start - starting point, the point at which the quad starts.
 % N       - the amount of trajectory points at the mth stage.
 % s_end   - the s_end point of the trajectory between the communication user and the
@@ -26,16 +26,96 @@ S_init(:,1:N) = s_start + V_str.*(1:N).*(s_end - s_start)./norm(s_end - s_start)
 % check if the calculated trajectory is valid. if not, calculate a new one.
 s_e = S_init(:,end); % get last position at the initial trajectory
 
-% read comments in first if statement to get an idea what happens in each
-% one of them
-if s_e(1) > L_x                                 % x position x out of bounds
+
+if (s_e(1) > L_x)&&(s_e(2) > L_y)                                    % x position x out of bounds
+    
+    if (abs(s_e-[L_x; s_e(2)]) > abs(s_e-[s_e(1); L_y]))
+        x_new  = L_x;                                                    % calc new x
+        s_e(1) = x_new;                             % calc new x
+        grad   = s_start - s_end;                   % calc diff to obtain slope
+        grad   = grad(2)./grad(1);                  % calc slop
+        y_new  = s_e(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
+        s_e(2) = y_new;                             % set new x
+    else
+        y_new     = L_y;
+        s_e(2)    = y_new;
+        grad_inv  = s_start - s_end;
+        grad_inv  = grad_inv(1)./grad_inv(2);
+        x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
+        s_e(1)    = x_new;
+    end
+    
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif (s_e(1) > L_x)&&(s_e(2) < 0)
+    if (abs(s_e-[L_x; s_e(2)]) > abs(s_e-[s_e(1); 0]))
+        x_new  = L_x;                                                    % calc new x
+        s_e(1) = x_new;                             % calc new x
+        grad   = s_start - s_end;                   % calc diff to obtain slope
+        grad   = grad(2)./grad(1);                  % calc slop
+        y_new  = s_e(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
+        s_e(2) = y_new;                             % set new x
+    else
+        y_new     = 0;
+        s_e(2)    = y_new;
+        grad_inv  = s_start - s_end;
+        grad_inv  = grad_inv(1)./grad_inv(2);
+        x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
+        s_e(1)    = x_new;
+    end
+    
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif (s_e(1) < 0)&&(s_e(2) > L_y)
+    if (abs(s_e-[0; s_e(2)]) > abs(s_e-[s_e(1); L_y]))
+        x_new  = 0;                                                    % calc new x
+        s_e(1) = x_new;                             % calc new x
+        grad   = s_start - s_end;                   % calc diff to obtain slope
+        grad   = grad(2)./grad(1);                  % calc slop
+        y_new  = s_e(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
+        s_e(2) = y_new;                             % set new x
+    else
+        y_new     = L_y;
+        s_e(2)    = y_new;
+        grad_inv  = s_start - s_end;
+        grad_inv  = grad_inv(1)./grad_inv(2);
+        x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
+        s_e(1)    = x_new;
+    end
+    
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif (s_e(1)  < 0)&&(s_e(2)  < 0)
+    if (abs(s_e-[0; s_e(2)]) > abs(s_e-[s_e(1); 0]))
+        x_new  = 0;                                                    % calc new x
+        s_e(1) = x_new;                             % calc new x
+        grad   = s_start - s_end;                   % calc diff to obtain slope
+        grad   = grad(2)./grad(1);                  % calc slop
+        y_new  = s_e(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
+        s_e(2) = y_new;                             % set new x
+    else
+        y_new     = 0;
+        s_e(2)    = y_new;
+        grad_inv  = s_start - s_end;
+        grad_inv  = grad_inv(1)./grad_inv(2);
+        x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
+        s_e(1)    = x_new;
+    end
+    
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
+elseif s_e(1) > L_x
     x_new  = L_x;                               % calc new x
     s_e(1) = x_new;                             % calc new x
     grad   = s_start - s_end;                   % calc diff to obtain slope
-    grad   = grad(2)./grad(1);                  % calc slop 
+    grad   = grad(2)./grad(1);                  % calc slop
     y_new  = s_end(2) + grad.*(x_new-s_e(1));   % calc y (no need to check boundary since x boundary is checked, and modification is linear)
     s_e(2) = y_new;                             % set new x
-
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
     S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N); % obtain linear trajectory. Note velocity is different than input
     S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
 elseif s_e(1) < 0
@@ -45,6 +125,7 @@ elseif s_e(1) < 0
     grad   = grad(2)./grad(1);
     y_new  = s_end(2) + grad.*(x_new-s_e(1));
     s_e(2) = y_new;
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
     
     S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
     S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
@@ -55,20 +136,21 @@ elseif s_e(2) > L_y
     grad_inv  = grad_inv(1)./grad_inv(2);
     x_new     = s_end(1) + grad_inv.*(y_new-s_e(2));
     s_e(1)    = x_new;
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
     
     S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
     S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
-elseif s_e(2) < 0 
+elseif s_e(2) < 0
     y_new    = 0;
     s_e(2)   = y_new;
     grad_inv = s_start - s_end;
     grad_inv = grad_inv(1)./grad_inv(2);
     x_new    = s_end(1) + grad_inv.*(y_new-s_e(2));
     s_e(1)   = x_new;
-     
-   S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
-   S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
-
+    V_str = norm(s_e-s_start)./((N+1).*T_f);
+    
+    S_init(1,:) = linspace(s_start(1) + V_str.*T_f,s_e(1), N);
+    S_init(2,:) = linspace(s_start(2) + V_str.*T_f,s_e(2), N);
 end
 
 end
