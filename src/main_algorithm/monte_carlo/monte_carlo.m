74d97a88620c915f4a428474b201c30533745432
sAz-G <66619540+sAz-G@users.noreply.github.com>
2023-04-11 18:23:57 +0200

    update monte_carlo
    generate positions before the mc simulations.
    
    add mse results

--- src/main_algorithm/monte_carlo/monte_carlo.m
+++ src/main_algorithm/monte_carlo/monte_carlo.m
@@ -1,6 +1,7 @@
 function results_mc = monte_carlo(params, setup, number_mc_iterations)
 %MONTO_CARLO Summary of this function goes here
 %   Detailed explanation goes here
+MSE_mc = zeros(1, number_mc_iterations);
 CRB_mc = zeros(1, number_mc_iterations);
 Rate_mc = zeros(1, number_mc_iterations);
 
@@ -9,13 +10,14 @@ p = gcp('nocreate');
 tic
 %% Do the main Monte-Carlo-Simulation
 parfor mc_iter = 1:number_mc_iterations
-    setup_local = setup;
+    setup_local  = setup;
     params_local = params;
     fprintf('   Monte-Carlo: n = %.f/%.f\n', mc_iter, number_mc_iterations);
 
     % generate random communication and sensing target
-    setup_local.comm_user_pos = [params_local.sim.L_x * rand(1); params_local.sim.L_y * rand(1)];
-    setup_local.sense_target_pos = [params_local.sim.L_x * rand(1); params_local.sim.L_y * rand(1)];
+    setup_local.comm_user_pos = setup.comm_user_pos(:,mc_iter);
+    setup_local.sense_target_pos = setup.sense_target_pos(:,mc_iter);
+    setup_local.est_sense_target = setup.est_sense_target(:,mc_iter);
 
     % run the multi_stage algorithm for a random setup
     
@@ -29,13 +31,15 @@ parfor mc_iter = 1:number_mc_iterations
     mc_hover_traj = reshape(mc_hover_traj(:, :, 1:res.M), 2, []); % check when incomplete last stage
 
     number_setpoints = length(mc_traj(1, :)); % check when incomplete last stage
-
-    CRB_mc(mc_iter) = crb(mc_hover_traj, setup_local.sense_target_pos, params);
-    Rate_mc(mc_iter) = avg_data_rate(mc_traj, setup_local.comm_user_pos, params, number_setpoints);
+    
+    MSE_mc(mc_iter)  = mean(norms(res.S_target_est_mat(:, ~isnan(res.S_target_est_mat(1,:)))-setup.sense_target_pos(:,number_mc_iterations),2,1).^2);
+    CRB_mc(mc_iter)  = crb(mc_hover_traj, setup_local.sense_target_pos, params);
+    Rate_mc(mc_iter) = avg_data_rate(mc_traj, setup_local.comm_user_pos, params, size(mc_traj,2));
 end
 mc_runtime = toc;
 
-results_mc = struct('CRB_avg', {mean(CRB_mc)}, ...
+results_mc = struct('CRB_avg',  {mean(CRB_mc)}, ...
                     'Rate_avg', {mean(Rate_mc)}, ...
+                    'MSE_avg', {mean(MSE_mc)}, ...
                     'Runtime_avg', {mc_runtime/number_mc_iterations});
 end
